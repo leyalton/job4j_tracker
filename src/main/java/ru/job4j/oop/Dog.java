132cea454399f44bdfdd9d8728900d074bbeae6f
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-11-10 00:39:35 +0300

    1.2. Вызов метода объекта[166085#264234]

--- src/main/java/ru/job4j/oop/Dog.java
+++ src/main/java/ru/job4j/oop/Dog.java
@@ -5,6 +5,5 @@ public class Dog {
         Dog dog1 = new Dog();
         Dog dog2 = new Dog();
         Dog dog3 = new Dog();
-
     }
 }
---
+++ src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,12 @@
+package ru.job4j.oop;
+
+public class Student {
+    public void song(){
+        System.out.println("I believe I can fly");
+    }
+
+    public static void main(String[] args) {
+        Student patric = new Student();
+        patric.song();
+    }
+}
