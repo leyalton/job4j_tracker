5557145d63614e35ca599f03cbb26577474811bc
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-11-10 00:42:16 +0300

    1.3. Вызов метода с аргументами.[166086#264235]

---
+++ src/main/java/ru/job4j/oop/Jukebox.java
@@ -0,0 +1,11 @@
+package ru.job4j.oop;
+
+public class Jukebox {
+    public void music(int position){
+        if (position == 1){
+            System.out.println("1");
+        }else if (position == 2){
+            System.out.println("2");
+        }
+    }
+}
