8a0fee7ddf6c2631a27efb813727955118b3b31f
leyalton <40add97a1d1d19a682a909c96@gmail.com>
2020-11-12 17:16:54 +0300

    1.5. Поля объекта[166083#264233]

---
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Cat {
+
+    private String food;
+    private String name;
+
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public static void main(String[] args) {
+        Cat gav = new Cat();
+        gav.eat("food");
+        gav.giveNick("name");
+        gav.show();
+    }
+}
